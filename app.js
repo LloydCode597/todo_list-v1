6173296ad17db63213bde6bcbf68bae7e93477c8
Lloyd Allen <lloydallen597@gmail.com>
2023-08-23 01:14:57 -0500

    Passing Data from the website to the Server and Getting the to-do list Function Working

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3003; // You can choose any available port number
+const port = 3006; // You can choose any available port number
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 // Set up body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,36 +13,27 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   var today = new Date();
-  var currentDay = today.getDay(); // Get the day of the week (0 is Sunday, 1 is Monday, and so on)
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", { kindOfDay: day });
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items,
+  });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 // Start the server
