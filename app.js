d4aa129ed0b18770335284265f09cef658e0a398
Lloyd Allen <lloydallen597@gmail.com>
2023-08-23 20:35:39 -0500

    Added Pre-Made CSS Stylesheets to Your Website

--- app.js
+++ app.js
@@ -2,25 +2,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3006; // You can choose any available port number
+const port = 3000; // You can choose any available port number
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 // Set up body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     kindOfDay: day,
@@ -29,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 
